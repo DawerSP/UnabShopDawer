2cbe39812247030a6e3bad9daa598675b64dedcf
Dawer Patiño <dpatino526@unab.edu.co>
2025-10-15 09:53:33 -0500

    set up navigation

--- app/src/main/java/co/edu/unab/dawerpatino/unabshopdawer/MainActivity.kt
+++ app/src/main/java/co/edu/unab/dawerpatino/unabshopdawer/MainActivity.kt
@@ -11,6 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import co.edu.unab.dawerpatino.unabshopdawer.ui.theme.HomeScreen
+import co.edu.unab.dawerpatino.unabshopdawer.ui.theme.LoginScreen
+import co.edu.unab.dawerpatino.unabshopdawer.ui.theme.RegisterScreen
 import co.edu.unab.dawerpatino.unabshopdawer.ui.theme.UnabShopDawerTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +24,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            UnabShopDawerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+            val navController = rememberNavController()
+            val startDestination = "login"
+            NavHost(navController, startDestination) {
+                composable(route = "login") {
+                    LoginScreen()
+                }
+                composable(route = "Register") {
+                    RegisterScreen()
+                }
+                composable(route = "Home") {
+                    HomeScreen()
                 }
             }
+
+
+            RegisterScreen()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UnabShopDawerTheme {
-        Greeting("Android")
-    }
-}
